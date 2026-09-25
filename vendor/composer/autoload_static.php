113a0e9726ad07342ccc0fb50b57da509a0102bb
Nils Hörrmann <nils.hoerrmann@hananils.de>
2026-09-25 09:28:27 +0200

    Update composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitc4f3816fccc5d0b58d3eab9875f6484d
 {
+    public static $prefixLengthsPsr4 = array (
+        'K' =>
+        array (
+            'Kirby\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Kirby\\' =>
+        array (
+            0 => __DIR__ . '/..' . '/getkirby/composer-installer/src',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -13,6 +27,8 @@ class ComposerStaticInitc4f3816fccc5d0b58d3eab9875f6484d
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitc4f3816fccc5d0b58d3eab9875f6484d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitc4f3816fccc5d0b58d3eab9875f6484d::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInitc4f3816fccc5d0b58d3eab9875f6484d::$classMap;
 
         }, null, ClassLoader::class);
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -10,6 +10,15 @@
         'dev' => true,
     ),
     'versions' => array(
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'hananils/kirby-type-methods' => array(
             'pretty_version' => '2.0.0',
             'version' => '2.0.0.0',
